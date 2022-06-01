b78e3645be6193f69b36e9c1c13f6b3bfaa97a9b
WM <34175525+ebicep@users.noreply.github.com>
2022-06-01 05:07:40 -0400

    initial commit

---
+++ src/main/java/com/ebicep/warlordspartymanager/WarlordsPartyManager.java
@@ -0,0 +1,18 @@
+package com.ebicep.warlordspartymanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WarlordsPartyManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
+
---
+++ src/main/java/com/ebicep/warlordspartymanager/WarlordsPartyManagerAPI.java
@@ -0,0 +1,7 @@
+package com.ebicep.warlordspartymanager;
+
+public interface WarlordsPartyManagerAPI {
+
+
+
+}
